a2dd1ff5b937fe8a44f32a643495e799fe5938f9
ungiback <38012855+ungiback@users.noreply.github.com>
2021-03-29 23:26:44 +0900

    Update and rename Img_to_txt.py to 텍스트_추출/Img_to_txt.py

--- 텍스트_추출/Img_to_txt.py
+++ 텍스트_추출/Img_to_txt.py
@@ -10,7 +10,7 @@ path_ = '이미지가있는 폴더경로'
 for i in os.listdir(path_):
     origin_img = cv2.imread(os.path.join(path_,i),cv2.IMREAD_UNCHANGED)
     cut_img = origin_img[200:234,1150:1487].copy() #전체이미지에서 원하는 부분 자르기
-    text = pytesseract.image_to_string(cut_img, lang='eng')
+    text = pytesseract.image_to_string(cut_img, lang='eng') #lang='kor'
     print(i.split('.')[0])
     if len(text.split(' ')) < 3:
         print('-')
@@ -19,3 +19,6 @@ for i in os.listdir(path_):
             print(text.split(' ')[2][0:2])
         else:
             print(text.split(' ')[2][0:3])
+
+            
+            
