ad4e5f5ee8f787514d2fbe8e086f7b0b9ab09c06
ungiback <38012855+ungiback@users.noreply.github.com>
2021-03-29 23:30:00 +0900

    Update Img_to_txt.py

--- 텍스트_추출/Img_to_txt.py
+++ 텍스트_추출/Img_to_txt.py
@@ -6,7 +6,7 @@ import os
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
 path_ = '이미지가있는 폴더경로'
-#C:/Users/ungi3/PycharmProjects/pythonProject/test_img
+
 for i in os.listdir(path_):
     origin_img = cv2.imread(os.path.join(path_,i),cv2.IMREAD_UNCHANGED)
     cut_img = origin_img[200:234,1150:1487].copy() #전체이미지에서 원하는 부분 자르기
